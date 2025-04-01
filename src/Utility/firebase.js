1930a6f509e9d41b0c1d8f07a54e5497747b4d6e
Aihabesha <ai.habesha.2025@gmail.com>
2025-04-01 00:43:54 -0700

    Save changes before cleaning history

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,10 +1,14 @@
-// Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import { getAuth } from "firebase/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-// Your web app's Firebase configuration using environment variables
+// Ensure environment variables are available
+if (!import.meta.env.VITE_FIREBASE_API_KEY) {
+  console.error("Missing Firebase API key in environment variables.");
+}
+
+// Firebase configuration using environment variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -18,3 +22,4 @@ const firebaseConfig = {
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = app.firestore();
+export default app;
