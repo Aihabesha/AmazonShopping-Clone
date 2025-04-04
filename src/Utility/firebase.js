6534cb5bc32d2e496112a016228607bdfd782fca
Aihabesha <ai.habesha.2025@gmail.com>
2025-04-04 03:19:22 -0700

    update .env

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,9 +1,8 @@
 import firebase from "firebase/compat/app";
-import { getAuth } from "firebase/auth";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -13,6 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 };
 
+// Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = app.firestore();
+export const auth = firebase.auth();
+export const db = firebase.firestore();
